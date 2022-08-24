b6d089ea1562acc32d2ab529a419adfb1f63a1ef
Aleyna Celik <clkkaley@gmail.com>
2022-08-24 13:41:11 +0300

    generated movies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [ //!component
     AppComponent,
     NavbarComponent,
-    CategoryComponent
+    CategoryComponent,
+    MoviesComponent
   ],
   imports: [ //!module
     BrowserModule
