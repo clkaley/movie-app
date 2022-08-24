31bee2345b38452c90d854d57bf42f7581485573
Aleyna Celik <clkkaley@gmail.com>
2022-08-24 13:39:23 +0300

    generated category component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [ //!component
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CategoryComponent
   ],
   imports: [ //!module
     BrowserModule
