fbc4c15212ccbc8a8a2c6899b3700b15ee5b8671
Aleyna Celik <clkkaley@gmail.com>
2022-08-25 17:44:16 +0300

    deleted pipe (filtered-movie) and added event (filtered movie)

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -10,19 +10,26 @@ import { MovieRepository } from '../models/movie.repository';
 export class MoviesComponent implements OnInit {
   title="hello title";
   movies:Movie[];
-  popularMovies:Movie[];
   movieRepository:MovieRepository;
-
+  filteredMovies:Movie[];
   filterText:string=""
 
 
   constructor() {
     this.movieRepository=new MovieRepository();
     this.movies=this.movieRepository.getMovies();
-    this.popularMovies=this.movieRepository.getPopularMovies();
+    this.filteredMovies=this.movies;
    }
 
   ngOnInit(): void {
+  }
+
+  onInputChange(){
+    console.log("onInputChange",this.filterText);
+    this.filteredMovies=this.filterText?
+    this.movies.filter
+    (m=>m.title.toLowerCase().indexOf(this.filterText)!==-1 || 
+        m.description.toLowerCase().indexOf(this.filterText)!==-1) : this.movies
   }
 
   // movies=["Film 1 ","Film 2","Film 3","Film 4"]
